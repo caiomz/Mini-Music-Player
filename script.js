4705bf7d28f83d603950704c7fd7a2171ac811ff
caio <caioabraao10@gmail.com>
2024-06-11 17:35:51 -0300

    Projeto Concluído

---
+++ script.js
@@ -0,0 +1,121 @@
+const songList = [{
+    name: "Tudo que você quiser",
+    artist: "Luan Santana",
+    src: "assets/1.mp3",
+    cover: "assets/1.jpg"
+},
+{
+    name: "Chuva de arroz",
+    artist: "Luan Santana",
+    src: "assets/2.mp3",
+    cover: "assets/2.jpg"
+},
+{
+    name: "Alas",
+    artist: "Soy Luna",
+    src: "assets/3.mp3",
+    cover: "assets/3.jpg"
+},
+{
+    name: "Better in Stereo",
+    artist: "Liv and Maddie",
+    src: "assets/4.mp3",
+    cover: "assets/4.jpg"
+},
+{
+    name: "Perfect",
+    artist: "Ed Sheeran",
+    src: "assets/5.mp3",
+    cover: "assets/5.jpg"
+},
+{
+    name: "Hino do Corinthians",
+    artist: "Hino do Corinthians",
+    src: "assets/6.mp3",
+    cover: "assets/6.jpg"
+}]
+
+const artistName = document.querySelector('.artist-name');
+const musicName = document.querySelector('.song-name');
+const fillBar = document.querySelector('.fill-bar');
+const time = document.querySelector('.time');
+const cover = document.getElementById('cover');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const prog = document.querySelector('.progress-bar');
+
+let song = new Audio();
+let currentSong = 0;
+let playing = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadSong(currentSong);
+    song.addEventListener('timeupdate', updateProgress);
+    song.addEventListener('ended', nextSong);
+    prevBtn.addEventListener('click', prevSong);
+    nextBtn.addEventListener('click', nextSong);
+    playBtn.addEventListener('click', togglePlayPause);
+    prog.addEventListener('click', seek);
+});
+
+function loadSong(index) {
+    const { name, artist, src, cover: thumb } = songList[index];
+    artistName.innerText = artist;
+    musicName.innerText = name;
+    song.src = src;
+    cover.style.backgroundImage = `url(${thumb})`;
+}
+
+function updateProgress() {
+    if (song.duration) {
+        const pos = (song.currentTime / song.duration) * 100;
+        fillBar.style.width = `${pos}%`
+
+        const duration = formatTime(song.duration);
+        const currentTime = formatTime(song.currentTime);
+        time.innerText = `${currentTime} - ${duration}`;
+    }
+}
+
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+    return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
+}
+
+function togglePlayPause() {
+    if (playing) {
+        song.pause();
+    } else {
+        song.play();
+    }
+    playing = !playing;
+    playBtn.classList.toggle('fa-pause', playing);
+    playBtn.classList.toggle('fa-play', !playing);
+    cover.classList.toggle('active', playing);
+}
+
+function nextSong() {
+    currentSong = (currentSong + 1) % songList.length;
+    playMusic();
+}
+
+function prevSong() {
+    currentSong = (currentSong - 1 + songList.length) % songList.length;
+    playMusic();
+}
+
+function playMusic() {
+    loadSong(currentSong);
+    song.play();
+    playing = true;
+    playBtn.classList.add('fa-pause');
+    playBtn.classList.remove('fa-play');
+    cover.classList.add('active');
+}
+
+function seek(e) {
+    const pos = (e.offsetX / prog.clientWidth) * song.duration;
+    song.currentTime = pos;
+}
